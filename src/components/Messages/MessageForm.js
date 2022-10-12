9e2676910c59dad2810508deb22ff5e12ba7eb99
yournewgoodname <damislav@net.hr>
2022-10-12 15:30:06 +0200

    finished tutorial

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,11 +2,12 @@ import React from "react";
 import uuidv4 from "uuid/v4";
 import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
+import { Picker, emojiIndex } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 import FileModal from "./FileModal";
 import ProgressBar from "./ProgressBar";
-import { EmojiPicker, emojiIndex, Picker } from "emoji-mart";
-import "emoji-mart/css/emoji-mart.css";
+
 class MessageForm extends React.Component {
   state = {
     storageRef: firebase.storage().ref(),
@@ -22,6 +23,13 @@ class MessageForm extends React.Component {
     modal: false,
     emojiPicker: false,
   };
+
+  componentWillUnmount() {
+    if (this.state.uploadTask !== null) {
+      this.state.uploadTask.cancel();
+      this.setState({ uploadTask: null });
+    }
+  }
 
   openModal = () => this.setState({ modal: true });
 
@@ -45,126 +53,10 @@ class MessageForm extends React.Component {
     }
   };
 
-  createMessage = (fileUrl = null) => {
-    const message = {
-      timestamp: firebase.database.ServerValue.TIMESTAMP,
-      user: {
-        id: this.state.user.uid,
-        name: this.state.user.displayName,
-        avatar: this.state.user.photoURL,
-      },
-    };
-    if (fileUrl !== null) {
-      message["image"] = fileUrl;
-    } else {
-      message["content"] = this.state.message;
-    }
-    return message;
-  };
-
-  sendMessage = () => {
-    const { getMessagesRef } = this.props;
-    const { message, channel, user, typingRef } = this.state;
-
-    if (message) {
-      this.setState({ loading: true });
-      getMessagesRef()
-        .child(channel.id)
-        .push()
-        .set(this.createMessage())
-        .then(() => {
-          this.setState({ loading: false, message: "", errors: [] });
-          typingRef.child(channel.id).child(user.uid).remove();
-        })
-        .catch((err) => {
-          console.error(err);
-          this.setState({
-            loading: false,
-            errors: this.state.errors.concat(err),
-          });
-        });
-    } else {
-      this.setState({
-        errors: this.state.errors.concat({ message: "Add a message" }),
-      });
-    }
-  };
-
-  getPath = () => {
-    if (this.props.isPrivateChannel) {
-      return `chat/private-${this.state.channel.id}`;
-    } else {
-      return "chat/public";
-    }
-  };
-
-  uploadFile = (file, metadata) => {
-    const pathToUpload = this.state.channel.id;
-    const ref = this.props.getMessagesRef();
-    const filePath = `${this.getPath()}/${uuidv4()}.jpg`;
-
-    this.setState(
-      {
-        uploadState: "uploading",
-        uploadTask: this.state.storageRef.child(filePath).put(file, metadata),
-      },
-      () => {
-        this.state.uploadTask.on(
-          "state_changed",
-          (snap) => {
-            const percentUploaded = Math.round(
-              (snap.bytesTransferred / snap.totalBytes) * 100
-            );
-            this.setState({ percentUploaded });
-          },
-          (err) => {
-            console.error(err);
-            this.setState({
-              errors: this.state.errors.concat(err),
-              uploadState: "error",
-              uploadTask: null,
-            });
-          },
-          () => {
-            this.state.uploadTask.snapshot.ref
-              .getDownloadURL()
-              .then((downloadUrl) => {
-                this.sendFileMessage(downloadUrl, ref, pathToUpload);
-              })
-              .catch((err) => {
-                console.error(err);
-                this.setState({
-                  errors: this.state.errors.concat(err),
-                  uploadState: "error",
-                  uploadTask: null,
-                });
-              });
-          }
-        );
-      }
-    );
-  };
-
-  sendFileMessage = (fileUrl, ref, pathToUpload) => {
-    ref
-      .child(pathToUpload)
-      .push()
-      .set(this.createMessage(fileUrl))
-      .then(() => {
-        this.setState({ uploadState: "done" });
-      })
-      .catch((err) => {
-        console.error(err);
-        this.setState({
-          errors: this.state.errors.concat(err),
-        });
-      });
-  };
   handleTogglePicker = () => {
-    this.setState({
-      emojiPicker: !this.state.emojiPicker,
-    });
+    this.setState({ emojiPicker: !this.state.emojiPicker });
   };
+
   handleAddEmoji = (emoji) => {
     const oldMessage = this.state.message;
     const newMessage = this.colonToUnicode(` ${oldMessage} ${emoji.colons} `);
@@ -186,6 +78,7 @@ class MessageForm extends React.Component {
       return x;
     });
   };
+
   createMessage = (fileUrl = null) => {
     const message = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
@@ -202,6 +95,7 @@ class MessageForm extends React.Component {
     }
     return message;
   };
+
   sendMessage = () => {
     const { getMessagesRef } = this.props;
     const { message, channel, user, typingRef } = this.state;
@@ -229,9 +123,10 @@ class MessageForm extends React.Component {
       });
     }
   };
+
   getPath = () => {
     if (this.props.isPrivateChannel) {
-      return `chat/private-${this.state.channel.id}`;
+      return `chat/private/${this.state.channel.id}`;
     } else {
       return "chat/public";
     }
@@ -283,6 +178,7 @@ class MessageForm extends React.Component {
       }
     );
   };
+
   sendFileMessage = (fileUrl, ref, pathToUpload) => {
     ref
       .child(pathToUpload)
--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -19,7 +19,11 @@ class DirectMessages extends React.Component {
       this.addListeners(this.state.user.uid);
     }
   }
-
+  removeListener = () => {
+    this.state.usersRef.off();
+    this.state.presenceRef.off();
+    this.state.connectedRef.off();
+  };
   addListeners = (currentUserUid) => {
     let loadedUsers = [];
     this.state.usersRef.on("child_added", (snap) => {
